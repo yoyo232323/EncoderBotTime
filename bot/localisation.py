7a909f1e959d51335748393549c49f4866b02b7d
Nirusaki <83818633+Nirusaki@users.noreply.github.com>
2021-10-02 13:35:35 +0530

    Update localisation.py
    New Messages

--- bot/localisation.py
+++ bot/localisation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AbirHasan2005
+# (c) @NIRUSAKI
 
 from bot.get_cfg import get_config
 
 
 class Localisation:
-    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Encoder Bot</b>. \n\n<b>Please send me any Telegram big video file I will compress it as s small video file!</b> \n\n/help for more details. \n\nChannel : @Animes_Encoded"
+    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Encoder Bot</b>. \n\n<b>Please send me any Telegram big video file I will compress it As small video file!</b> \n\n/help for more details. \n\nChannel : @Animes_Encoded"
    
     ABS_TEXT = " Please don't be selfish."
     
@@ -23,7 +23,7 @@ class Localisation:
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "©️ @Animes_Encoded"
+    COMPRESS_SUCCESS = "©️ @FIERCENETWORK"
 
     COMPRESS_PROGRESS = "⏳ ETA: {}\n🚀 Progress: {}%"
 
@@ -47,7 +47,7 @@ class Localisation:
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
-        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nSupport Group: @Linux_Repo"
+        "Hi, I am Video Compressor Bot \n\n1. Send me your telegram big video file \n2. Reply to the file with: `/compress 50` \n\nSupport Group: @FIERCENETWORK"
     )
     WRONG_MESSAGE = get_config(
         "STRINGS_WRONG_MESSAGE",
