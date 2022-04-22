a74a8ea09e50228c0643852372ab485b97ebf6f2
mradulkumar-glitch <84231956+mradulkumar-glitch@users.noreply.github.com>
2022-04-22 13:33:03 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -79,6 +79,4 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
-app.send_message(
-    chat_id=AUTH_USERS, "**I AM UP 😎**"
-)
+app.send_message(chat_id=AUTH_USERS, "**I AM UP 😎**")
