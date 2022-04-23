398b9a3a7b13cfca4a444ddd08fef968cc9f8da1
mradulkumar-glitch <84231956+mradulkumar-glitch@users.noreply.github.com>
2022-04-23 08:24:21 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -47,7 +47,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
     bit.append("yuv420p")
     preset.append("")
     watermark.append("")
-    file_genertor_command = f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}"  -i "https://te.legra.ph/file/e9408e71281cdcb017874.png" -map 0 -filter_complex "overlay =main_w-(overlay_w+10):main_h-(overlay_h+10)"  -c:v libx265 -crf {crf[0]} -c:s copy -s {resolution[0]} -preset faster -metadata title="Visit For More Movies [t.me/AniXpo]"  -metadata:s:v title="Visit Website[Anixpo] t.me/AniXpo] - 480p - HEVC - 8bit"  -metadata:s:a title="[Visit t.me/AniXpo] - Opus - 54 kbps" -metadata:s:s title="[AniXpo Substations Alpha]" -c:a libopus -ab 64k "{out_put_file_name}" -y'
+    file_genertor_command = f'ffmpeg -hide_banner -loglevel quiet -progress "{progress}" -i "{video_file}"  -i "https://te.legra.ph/file/e9408e71281cdcb017874.png" -map 0 -filter_complex "overlay =main_w-(overlay_w+10):main_h-(overlay_h+10)"  -c:v libx265 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -crf {crf[0]} -c:s copy -s {resolution[0]} -preset faster -metadata title="Visit For More Movies [t.me/AniXpo]"  -metadata:s:v title="Visit Website[Anixpo] t.me/AniXpo] - 480p - HEVC - 8bit"  -metadata:s:a title="[Visit t.me/AniXpo] - Opus - 54 kbps" -metadata:s:s title="[AniXpo Substations Alpha]" -c:a libopus -ab 40k -vbr 2 -ac 2 -level 3.1 "{out_put_file_name}" -y'
  #For Ffmpeg Use
     COMPRESSION_START_TIME = time.time()
     process = await asyncio.create_subprocess_shell(
