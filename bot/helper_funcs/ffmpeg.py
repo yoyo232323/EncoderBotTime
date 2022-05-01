aa473bfee3e00880617f7793457972f2d4cb0ebd
Mradul <nirusakimalaal@gmail.com>
2022-05-01 13:19:01 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -88,7 +88,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
         if difference > 0:
           ETA = TimeFormatter(difference*1000)
         percentage = math.floor(elapsed_time * 100 / total_time)
-        progress_str = "'┣<b>Percentage ⏰ :</b> {0}%\n[{1}{2}]".format(
+        progress_str = "<b>Percentage ⏰ :</b> {0}%\n[{1}{2}]".format(
             round(percentage, 2),
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))])
